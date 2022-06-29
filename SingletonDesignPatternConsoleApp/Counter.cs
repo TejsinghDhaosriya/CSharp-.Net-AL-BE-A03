756728b3ee20176856b10dfd9979659a9e43eae8
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-06-29 23:54:09 +0530

    singleton pattern

---
+++ SingletonDesignPatternConsoleApp/Counter.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonDesignPatternConsoleApp
+{
+    // Singleton Pattern - not thread safe
+    internal class Counter
+    {
+        private static int _count;
+        private static Counter _instance;
+
+        private Counter() { }
+
+        public static Counter GetInstance => _instance ?? (_instance = new Counter());
+
+        public void Increment() => _count++;
+
+        public void Decrement() => _count--;
+
+        public int Count() => _count;
+    }
+}
---
+++ SingletonDesignPatternConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+namespace SingletonDesignPatternConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = Counter.GetInstance;
+            WriteLine(counter.Count());
+
+            counter.Increment();
+            counter.Increment();
+            counter.Increment();
+            WriteLine(counter.Count());
+
+            counter.Decrement();
+            WriteLine(counter.Count());
+
+
+            //Singleton Check
+
+            Counter counter2 = Counter.GetInstance;
+            WriteLine(counter2.Count());
+            if (counter == counter2)
+                WriteLine("Both are Same");
+
+
+            Read();
+
+        }
+    }
+}
